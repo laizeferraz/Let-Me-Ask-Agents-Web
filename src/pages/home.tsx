6316257b007ebdbfa9190cb1f442b5cdaec232be
Laize Ferraz <laizeferraz@gmail.com>
2025-07-29 16:26:02 +1000

    add aside with illustration

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -17,7 +17,19 @@ export const Home = () => {
           />
         </Link>
       </header>
-      <div className="mx-auto grid min-h-screen max-w-4xl grid-cols-1 items-center justify-center p-4">
+      <div className="mx-auto grid min-h-screen max-w-4xl grid-cols-1 items-center justify-center gap-4 p-4 md:grid-cols-2">
+        <aside className="hidden flex-col md:flex">
+          <div
+            className="bg-contain bg-no-repeat p-40"
+            style={{ backgroundImage: 'url(/illustration.svg)' }}
+          />
+          <p className="mt-10 font-bold font-secondary text-4xl text-white-200 leading-10">
+            Create live Q&A rooms
+          </p>
+          <p className="mt-10 text-l text-white-200 leading-8">
+            Answer questions in real time with AI help.
+          </p>
+        </aside>
         <CreateRoomForm />
       </div>
     </div>
