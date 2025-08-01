d2739b4d3d3d9ecfa708bc6eb2bfbdc086948f31
Laize Ferraz <laizeferraz@gmail.com>
2025-08-01 18:14:08 +1000

    prepare for deployment

--- src/http/use-rooms.ts
+++ src/http/use-rooms.ts
@@ -1,12 +1,12 @@
 import { useQuery } from '@tanstack/react-query';
+import { API_URL } from '@/lib/api';
 import type { GetRoomsResponse } from './types/get-rooms-response';
 
 export function useRooms() {
   return useQuery({
     queryKey: ['get-rooms'],
     queryFn: async () => {
-      // Simulate an API call
-      const response = await fetch('http://localhost:3333/rooms');
+      const response = await fetch(`${API_URL}/rooms`);
       const result: GetRoomsResponse = await response.json();
       return result;
     },
